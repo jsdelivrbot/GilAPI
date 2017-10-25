12689492937bbde0dc53ecf1348d5d4a0865d3ed
Gilgamech <Gilgamech@Gilgamech.com>
2017-10-24 22:23:51 -0700

    Add fruitbot directory.

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get('/', function(request, response) {
 app.get('/port', function(request, response) {
   response.render('pages/port');
 });
+app.get('/fruitbot', function(request, response) {
+  response.render('pages/fruitbot');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
