b2be3cb463752bd3937fc8a743e20136412ab5bf
Gilgamech <Gilgamech@Gilgamech.com>
2017-10-24 19:00:33 -0700

    Cleanup of old files, testing update to nav header.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+// var http = require('http');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,3 +17,12 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+/*
+var server = http.createServer(function(req, res) {
+res.writeHead(200);
+res.end('Hi everybody!');
+});
+server.listen(80);
+*/
+
