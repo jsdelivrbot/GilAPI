fa24c8e9cca18604fa163d52c22b58000874f873
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-05 07:48:42 -0800

    Readd second Mongo trial, with different err handling.

--- index.js
+++ index.js
@@ -22,12 +22,88 @@ app.set('view engine', 'ejs');
 dbuser = process.env.DBUSER;
 dbpassword = process.env.DBPASS;
 
+var seedData = [
+  {
+    decade: '1970s',
+    artist: 'Debby Boone',
+    song: 'You Light Up My Life',
+    weeksAtOne: 10
+  },
+  {
+    decade: '1980s',
+    artist: 'Olivia Newton-John',
+    song: 'Physical',
+    weeksAtOne: 10
+  },
+  {
+    decade: '1990s',
+    artist: 'Mariah Carey',
+    song: 'One Sweet Day',
+    weeksAtOne: 16
+  }
+];
+
 // Connection mongoUrl
 var mongoUri = 'mongodb://' + dbuser + ':' + dbpassword +'@ds249325.mlab.com:49325/gilapi';
 
 // Use connect method to connect to the server
 mongodb.MongoClient.connect(mongoUri, function(err, db) {
   chatGeneral = chatGeneral + "Connected successfully to server\n\r";
+  if(err) chatGeneral = chatGeneral + err;
+  
+  /*
+   * First we'll add a few songs. Nothing is required to create the 
+   * songs collection; it is created automatically when we insert.
+   */
+
+  var songs = db.collection('songs');
+
+   // Note that the insert method can take either an array or a dict.
+
+  songs.insert(seedData, function(err, result) {
+    
+    if(err) chatGeneral = chatGeneral + err;
+
+    /*
+     * Then we need to give Boyz II Men credit for their contribution
+     * to the hit "One Sweet Day".
+     */
+
+    songs.update(
+      { song: 'One Sweet Day' }, 
+      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
+      function (err, result) {
+        
+        if(err) chatGeneral = chatGeneral + err;
+
+        /*
+         * Finally we run a query which returns all the hits that spend 10 or
+         * more weeks at number 1.
+         */
+
+        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
+
+          if(err) chatGeneral = chatGeneral +  err;
+
+          docs.forEach(function (doc) {
+            chatGeneral = chatGeneral + 
+              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
+              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.';
+          });
+         
+          // Since this is an example, we'll clean up after ourselves.
+          songs.drop(function (err) {
+            if(err) chatGeneral = chatGeneral +  err;
+           
+            // Only close the connection when your app is terminating.
+            db.close(function (err) {
+              if(err) chatGeneral = chatGeneral +  err;
+            }); // end db.close
+          }); // end songs.drop
+		}); // end insertDocuments
+      } // end function
+    ); // end songs.update
+  }; // end songs.insert
 }); // end MongoClient
 
 
