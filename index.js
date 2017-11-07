e0b38299aac908e00aa8a6f5e7d2a2d0661d0c64
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-06 16:51:11 -0800

    Readd routes and passport calls; remove duplicated passport code from index.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var LocalStrategy = require('passport-local').Strategy;
 var session = require("express-session");
 const { Client } = require('pg');
 
+require('./config/passport')(passport); // pass passport for configuration
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 var app = express();
 
 var chatGeneral = "";
@@ -45,28 +48,6 @@ client.query('SELECT table_name FROM information_schema.tables;', (err, queryOut
     chatGeneral = chatGeneral + row.table_name + "\r\n";
   }
   client.end();
-});
-
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
 });
 
 app.get('/', function(request, response) {
