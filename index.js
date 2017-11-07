51a623e6c896b465064048c5b9862560f999ea17
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-06 16:07:09 -0800

    Add Passport local strategy, serialize calls, and update login post call.

--- index.js
+++ index.js
@@ -50,16 +50,38 @@ client.query('SELECT table_name FROM information_schema.tables;', (err, queryOut
   client.end();
 });
 
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
-
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+  
 app.get('/logout', function(request, response){
   // console.log('logging out');
   request.logout();
