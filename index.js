473b6bd8924501e9dbe6e324bfd69ca2fb5bb7e3
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-07 15:27:10 -0800

    Readding Passport strategy and serial calls.

--- index.js
+++ index.js
@@ -47,6 +47,30 @@ client.query('SELECT table_name FROM information_schema.tables;', (err, queryOut
   client.end();
 });
 
+//Passport stuff
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+// Page calls
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
