2bc1820fec673801ae563ebceba44484c8654f3c
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-07 19:48:57 -0800

    Updating User call.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var Sequelize = require('sequelize');
 var pg = require('pg').native;
 var pghstore = require('pg-hstore');
 var sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://dbuser:dbpasswd@dbhost:5432/dbname');
-var User = sequelize.import('./app/user');
+var User = sequelize.import('./User');
 
 var app = express();
 
