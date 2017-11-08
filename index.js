cdabfb0ebaf151b784ff848a5fe8aaa52346d268
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-08 07:13:46 -0800

    Pull signup strategy.

--- index.js
+++ index.js
@@ -65,46 +65,6 @@ passport.use(new LocalStrategy(
   }
 ));
 
-// LOCAL SIGNUP ============================================================
-passport.use('local-signup', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-},function(req, email, password, done) {        
-    //  Whether we're signing up or connecting an account, we'll need
-    //  to know if the email address is in use.
-    
-    User.findOne({ where: { localemail: email }})
-        .then(function(existingUser) {
-        
-            // check to see if there's already a user with that email
-            if (existingUser) 
-                return done(null, false, {message:'loginMessage', 'That email is already taken.'});
-
-            //  If we're logged in, we're connecting a new local account.
-            if(req.user) {
-                var user            = req.user;
-                user.localemail    = email;
-                user.localpassword = User.generateHash(password);
-                user.save().catch(function (err) {
-                    throw err;
-                }).then (function() {
-                    done(null, user);
-                });
-            } 
-            //  We're not logged in, so we're creating a brand new user.
-            else {
-                // create the user
-                var newUser = User.build ({localemail: email, localpassword: User.generateHash(password)});    
-                newUser.save().then(function() {done (null, newUser);}).catch(function(err) { done(null, false, {message:'loginMessage', err});
-            }
-        })
-        .catch(function(e) {
-            done(null, false, {message:'loginMessage',e.name + " " + e.message});
-        })
-})};
-
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
