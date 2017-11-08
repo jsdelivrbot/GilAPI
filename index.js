3594c280b4e368cc2889c96ca50607edad917d0c
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-08 07:56:18 -0800

    Update local strategy error handling.

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ client.query('SELECT table_name FROM information_schema.tables;', (err, queryOut
 passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
+	  if (err) {chatGeneral = chatGeneral + err};
       if (!user) { return done(null, false); }
       if (!user.verifyPassword(password)) { return done(null, false); }
       return done(null, user);
