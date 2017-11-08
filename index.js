cc8e3d259dc26540c279ac4f2a54505701842139
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-07 21:45:45 -0800

    Reverting cosmetic updates.

--- index.js
+++ index.js
@@ -83,10 +83,10 @@ app.get('/', function(request, response) {
 app.get('/login', function(request, response) {
   response.render('pages/login');
 });
-app.post('/login', 
+app.post('/login',
   passport.authenticate('local', {
-	successRedirect : '/loginSuccess',
-	failureRedirect : '/loginFailure'
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
   })
 );
 
