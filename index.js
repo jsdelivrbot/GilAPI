da465328f1ea72f8c5085a4851e43582e89c864f
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-08 08:28:00 -0800

    Remove first testing DB call.

--- index.js
+++ index.js
@@ -50,18 +50,9 @@ client.query('SELECT table_name FROM information_schema.tables;', (err, queryOut
     chatGeneral = chatGeneral + row.table_name + "\r\n";
   }
 });
-client.query('SELECT * FROM Users;', (err, queryOutput) => {
-  if (err) chatGeneral = chatGeneral + err;
-  chatGeneral = chatGeneral + 'SELECT FROM Users\n\r';
-  for (let row of queryOutput.rows) {
-    chatGeneral = chatGeneral + row.table_name + "\r\n";
-  }
-  client.end();
-});
-
 client.query('SELECT * FROM users;', (err, queryOutput) => {
   if (err) chatGeneral = chatGeneral + err;
-  chatGeneral = chatGeneral + 'SELECT FROM users\n\r';
+  chatGeneral = chatGeneral + 'SELECT FROM Users\n\r';
   for (let row of queryOutput.rows) {
     chatGeneral = chatGeneral + row.table_name + "\r\n";
   }
