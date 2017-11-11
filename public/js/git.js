5633170b478f484df3a1985cab93a5d60c1f6b65
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2017-11-11 07:35:50 -0800

    Update git page logic, move JS logic from text2 to git file.

--- public/js/git.js
+++ public/js/git.js
@@ -20,10 +20,12 @@ function updateGitPage() {
   // If textbox not empty, push contents to cookie, otherwise push from cookie to textbox. Always push to name field.
   gitFileName = document.getElementById("gitFileName").value
   if (gitFileName) {
+    document.getElementById("gitFileName").value = gitFileName
   } else {
-    gitFileName = document.getElementById("gitFileName").value
+	  gitFileName = "README.MD"
+      document.getElementById("gitFileName").value = gitFileName
   }; //end if gitFileName
-  gitFileName = document.getElementById("gitFileNameItem").value
+  document.getElementById("gitFileNameItem").value = gitFileName
   
   // Load file from repo into gitFileTextArea.
   gitRepoUrl = document.getElementById("gitRepoUrl").value + "/" + gitFileName
@@ -32,3 +34,24 @@ function updateGitPage() {
   }); // end loadJSON
   
 }; // end updateForm
+
+
+function updateForm(nfsCall, nfsName, nfsTextArea) {
+  nfsInput = document.getElementById(nfsName).value
+  nfsurl = "https://gil-api.herokuapp.com/" + nfsCall + "?name=" + nfsInput
+  loadJSON(nfsurl, function(response) {
+    document.getElementById(nfsTextArea).value = response //actual_JSON
+  }); // end loadJSON
+}; // end updateForm
+
+function updateNFSForm(nfsCall, nfsName, nfsTextArea, nfsParams, nfsType) {
+  nfsName = document.getElementById("NFSpageName").value
+  nfsParams = document.getElementById("NFSInput").value
+  nfsurl = "https://gil-api.herokuapp.com/" + nfsCall + "?name=" + nfsName + "&params=" + nfsParams + "&type=" + nfsType
+  loadJSON(nfsurl, function(response) {
+    document.getElementById(nfsTextArea).value = response //actual_JSON
+  }); // end loadJSON
+}; // end updateForm
+
+updateForm('newappget', 'pageName', 'IndexJS')
+updateForm('newappget', 'NFSpageName', 'TestJS')
