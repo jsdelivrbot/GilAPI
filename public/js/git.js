6e6d329ce4fb79eac7a390ea222f07e792ddb148
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2017-11-11 08:32:11 -0800

    Add text saving field to Git page, to start to incorporate that functionality.

--- public/js/git.js
+++ public/js/git.js
@@ -16,6 +16,43 @@ function loadJSON(file, callback) {
     xobj.send(null);  
 };// end loadJSON
 
+// https://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
+function saveTextAsFile() {
+	var textToWrite = document.getElementById("inputTextToSave").value;
+	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+	var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null) {
+		// Chrome allows the link to be clicked
+		// without actually adding it to the DOM.
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	} else {
+		// Firefox requires the link to be added to the DOM
+		// before it can be clicked.
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
+	downloadLink.click();
+}
+
+function destroyClickedElement(event) {	document.body.removeChild(event.target); }
+
+function loadFileAsText() {
+	var fileToLoad = document.getElementById("fileToLoad").files[0];
+	var fileReader = new FileReader();
+	
+	fileReader.onload = function(fileLoadedEvent) {
+		var textFromFileLoaded = fileLoadedEvent.target.result;
+		document.getElementById("inputTextToSave").value = textFromFileLoaded;
+	};
+	fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
 function updateGitPage() {
   // If textbox not empty, push contents to cookie, otherwise push from cookie to textbox. Always push to name field.
   gitFileName = document.getElementById("gitFileName").value
