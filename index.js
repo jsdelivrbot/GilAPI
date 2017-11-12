89d463429d999e87162221520a5b1a4e735189a9
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2017-11-12 08:04:18 -0800

    Local Signup stuff readd.

--- index.js
+++ index.js
@@ -70,6 +70,54 @@ client.query('SELECT * FROM users;', (err, queryOutput) => {
 });
 
 //Passport stuff
+//Local Signup
+passport.use('signup', new LocalStrategy({
+    passReqToCallback : true
+  },
+  function(req, username, password, done) {
+    findOrCreateUser = function(){
+      // find a user in db with provided username
+      User.findOne({'username':username},function(err, user) {
+        // In case of any error return
+        if (err){
+          console.log('Error in SignUp: '+err);
+          return done(err);
+        }
+        // already exists
+        if (user) {
+          console.log('User already exists');
+          return done(null, false, 
+             req.flash('message','User Already Exists'));
+        } else {
+          // if there is no user with that email
+          // create the user
+          var newUser = new User();
+          // set the user's local credentials
+          newUser.username = username;
+          newUser.password = createHash(password);
+          newUser.email = req.param('email');
+          newUser.firstName = req.param('firstName');
+          newUser.lastName = req.param('lastName');
+ 
+          // save the user
+          newUser.save(function(err) {
+            if (err){
+              console.log('Error in Saving user: '+err);  
+              throw err;  
+            }
+            console.log('User Registration succesful');    
+            return done(null, newUser);
+          });
+        }
+      });
+    };
+     
+    // Delay the execution of findOrCreateUser and execute 
+    // the method in the next tick of the event loop
+    process.nextTick(findOrCreateUser);
+  });
+);
+
 // LOCAL LOGIN
 passport.use(new LocalStrategy(
   function(username, password, done) {
@@ -155,7 +203,7 @@ app.post('/signup', function (request, response) {
   
   client.query("INSERT INTO Users (localemail, localpassword) VALUES (userEmail_query, userPassword_query);", (err, queryOutput) => {
     if (err) chatGeneral = chatGeneral + err;
-    chatGeneral = chatGeneral + 'New User userEmail_query signup\n\r';
+    chatGeneral = chatGeneral + 'New User ' + userEmail_query + ' signup\n\r';
     for (let row of queryOutput.rows) {
       chatGeneral = chatGeneral + row + "\r\n";
     }
