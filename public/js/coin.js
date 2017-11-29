926cc55ed92837c660a782e666af77d343fa35f9
Gilgamech <Gilgamech@Gilgamech.com>
2017-11-29 11:03:48 -0800

    Rewrite coin page logic.

--- public/js/coin.js
+++ public/js/coin.js
@@ -1,58 +1,34 @@
-function refreshCoin(outputTextBox){
-  timeUrl = "https://api.coinbase.com/v2/time"
-  loadCoin(timeUrl,outputTextBox)
-  btcBuyUrl = "https://api.coinbase.com/v2/prices/BTC-USD/buy"
-  loadCoin(btcBuyUrl,outputTextBox)
-  ltcBuyUrl = "https://api.coinbase.com/v2/prices/LTC-USD/buy"
-  loadCoin(ltcBuyUrl,outputTextBox)
-  ethBuyUrl = "https://api.coinbase.com/v2/prices/ETH-USD/buy"
-  loadCoin(ethBuyUrl,outputTextBox)
-}; // end refreshChat
+var $btcOld
+var $ltcOld 
+var $ethOld 
+var $time
+var $btc
+var $ltc
+var $eth
 
-function loadCoin(coinUrl,coinBox){
-  loadJSON(coinUrl, function(response) {
-  if (response.data.iso) {
-      document.getElementById(coinBox).value += response.data.iso + lineBreak
-  } else {
-	  amount = response.data.amount
-      document.getElementById(coinBox).value += response.data.base + " | " + amount  + " | " + lineBreak 
-  }
-  }); // end loadJSON
-}; // end loadChat
+//Get data from URLs, add to array, do math against old array, output. 
+//Have function get data from URL and output.
+
+function refreshCoin (outputTextBox) {
+	loadJSON("https://api.coinbase.com/v2/time", function($response) { $time = $response.data});
+	loadJSON("https://api.coinbase.com/v2/prices/BTC-USD/buy", function($response) { $btc = $response.data});
+	loadJSON("https://api.coinbase.com/v2/prices/LTC-USD/buy", function($response) { $ltc = $response.data});
+	loadJSON("https://api.coinbase.com/v2/prices/ETH-USD/buy", function($response) { $eth = $response.data});
+	$coin2 = [];
+	$coin2 = $time.iso + lineBreak;
+	$coin2 += $eth.base + " | " + $eth.amount  + " | " + (Math.round(($eth.amount - $ethOld)*100)/100)+ lineBreak;
+	$coin2 += $ltc.base + " | " + $ltc.amount  + " | " + (Math.round(($ltc.amount - $ltcOld)*100)/100) + lineBreak;
+	$coin2 += $btc.base + " | " + $btc.amount  + " | " + (Math.round(($btc.amount - $btcOld)*100)/100)+ lineBreak;
+
+	document.getElementById(outputTextBox).value  = $coin2 + document.getElementById(outputTextBox).value
+
+	$btcOld = $btc.amount
+	$ltcOld = $ltc.amount
+	$ethOld = $eth.amount
+};
 
 // Refresh chart every 60 seconds.
 setInterval(function () {
   refreshCoin("coinMainBox")
 }, 60000);
 
-
-var $btcOld
-var $ltcOld 
-var $ethOld 
-
-function r2Coin (outputTextBox) {
-var $time
-var $btc
-var $ltc
-var $eth
-loadJSON("https://api.coinbase.com/v2/time", function($response) { $time = $response.data});
-loadJSON("https://api.coinbase.com/v2/prices/BTC-USD/buy", function($response) { $btc = $response.data});
-loadJSON("https://api.coinbase.com/v2/prices/LTC-USD/buy", function($response) { $ltc = $response.data});
-loadJSON("https://api.coinbase.com/v2/prices/ETH-USD/buy", function($response) { $eth = $response.data});
-$coin2 = [];
-$coin2 = $coin2 + $time + lineBreak;
-$coin2 += $eth.base + " | " + $eth.amount + lineBreak;
-$coin2 = $coin2 + $ltc + lineBreak;
-$coin2 = $coin2 + $btc + lineBreak;
-/*
-$coin2 += $time.iso + lineBreak;
-$coin2 += $eth.base + " | " + $eth.amount  + " | " + $ethOld+ lineBreak;
-$coin2 += $ltc.base + " | " + $ltc.amount  + " | " + $ltcOld+ lineBreak;
-$coin2 += $btc.base + " | " + $btc.amount  + " | " + $btcOld+ lineBreak;
-*/
-document.getElementById(outputTextBox).value  += $coin2
-/*
-$btcOld = $btc.amount
-$ltcOld = $ltc.amount
-$ethOld = $eth.amount*/
-};
