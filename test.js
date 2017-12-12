952745880527fa9bf37bff50dd1a173a851eef29
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2017-12-11 18:15:16 -0800

    Increment tests.

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(48);
+  t.plan(52);
 
   // Wait until the server is ready
   child.stdout.on('data', _ => {
