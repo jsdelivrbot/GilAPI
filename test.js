534604a7d259f9111655cc151c76174ed1a692b8
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-01-06 12:37:30 -0800

    Remove unnecessary tests.

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(52);
+  t.plan(50);
 
   // Wait until the server is ready
   child.stdout.on('data', _ => {
@@ -29,8 +29,6 @@ test('responds to requests', (t) => {
     request('http://127.0.0.1:5000/badpw', (error, response, body) => {
       t.false(error); // test 9
       t.equal(response.statusCode, 200); // test 10
-      t.notEqual(body.indexOf("<title>Gilgamech Technologies</title>"), -1); // test 11
-      t.notEqual(body.indexOf("Gilgamech Technologies"), -1); // test 12
     }); //end request
 	
     request('http://127.0.0.1:5000/meme', (error, response, body) => {
