fd1743b81892f693e20deb161b2f6401ae332398
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-01-26 15:06:58 -0800

    Remove favicon test.

--- test.js
+++ test.js
@@ -7,22 +7,17 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(4);
+  t.plan(2);
 
   // Wait until the server is ready
   child.stdout.on('data', _ => {
     // Make a request to our app
     request('http://127.0.0.1:5000', (error, response, body) => {
-      t.false(error); // test 1
-      t.equal(response.statusCode, 200); // test 2
-    }); //end request
-	
-    request('http://127.0.0.1:5000/favicon.ico', (error, response, body) => {
       // stop the server
       child.kill();
 
-      t.false(error); // test 3
-      t.equal(response.statusCode, 200); // test 4
+      t.false(error); // test 1
+      t.equal(response.statusCode, 200); // test 2
     }); //end request
 	
   }); //end child
