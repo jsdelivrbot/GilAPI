058afce0057a47a1833f54d87b65c24136ff70e8
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-01-26 16:31:44 -0800

    Remove extraneous plus symbol.

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get(/\S+/g, function(request, response) {
 	//https://gil-api.herokuapp.com/?p=giltech
 	var $queryString = request.path
 	if ($queryString == "/") {
-		$queryString += "/root"
+		$queryString = "/root"
 	}; //end if siteName
 	response.send('<!DOCTYPE html><html lang="en"><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="shortcut icon" href="' + $siteBase + '/favicon.ico" type="image/x-icon"><link href="' + $siteBase + '' + $queryString + $queryString + '.css" rel="stylesheet" type="text/css"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><div id="deleteme" hidden><p1>Page requires Javascript and load files (XHR) to function.</p1><br><p3>This page composes itself entirely from Javascript -  a true single-page application, not only is it entirely one page in the browser. Where most websites use HTML for structure, CSS for style, and Javascript for operations, this page uses JSON to express every element. This uses a small (less than 500 lines) Javascript engine to interpret the JSON. To see this in action, please permit the site to run Javascript, and load files from the data source: </p3><br><div id="pageSettingsJson" >' + $siteBase + '' + $queryString + $queryString + '.json</div></div></body></html><script src="' + $siteBase + '/Gilgamech.js"></script> ')
 });
