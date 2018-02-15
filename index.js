bcbb031c68bd5f0a1af1ac3df202f195fd728093
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-02-14 18:47:49 -0800

    Modify test some more.

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.post('/login', function(request, response) {
 }); // end app post login 
 
 app.post('/login2', function(request, response) {
-	var $r2 = JSON.stringify(request.body);
+	var $r2 = JSON.stringify(request.query);
 	response.send('login2 POST successful' + $r2);
 }); // end app post login 
 	  
