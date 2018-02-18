c0e41f18100420ad6ad9ad382783aae6b537046f
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-02-17 21:26:13 -0800

    Add loadJSON and postJSON.

--- index.js
+++ index.js
@@ -77,6 +77,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
 app.use(cookieParser()); // read cookies (needed for auth)
 
+function loadJSON(file, callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+          };
+    };
+    xobj.send(null);  
+};// end loadJSON
+
+function postJSON(file, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("text/plain");
+    xobj.open('POST', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(JSON.parse(xobj.responseText));
+          }
+    };
+    xobj.send(null);  
+};// end loadJSON
+
+
+
 function addErr(err) {
   $settingsVar.errgoLogic += err + "<br>"// lineBreak
 };
