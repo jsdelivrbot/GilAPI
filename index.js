d1aa58fb018ebcd7ee3fa319d4464676906471f8
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-02-18 11:02:45 -0800

    Add putObject call to write passwords after signup.

--- index.js
+++ index.js
@@ -156,6 +156,11 @@ app.post('/login', function(request, response) {
 		}; //end if err
 		$userPWHTable[$userName] = $hash
 		  
+s3.putObject({ Bucket: $privateBucket, Key: "userPWHTable.json", Body: JSON.stringify($urlPWHTable) }, function(err, data) {
+	if (err) {
+		addErr(err);
+	}; // end if err
+});		  
 		addErr(("User signup: " + $userName));
 		request.session.regenerate(function(){
 			request.session.user = $userName;
