c6602559fc209b7f2b5916c0c52109229648d373
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-02-19 13:15:24 -0800

    Modify debug call.

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ var $urlPWHParams = {
 	Key: 'userPWHTable.json'
 };
 $s3.getObject($urlPWHParams, function(err, dataStream){
-	$userPWHTable = dataStream.Body.toString('utf-8');
-	addErr(JSON.stringify($userPWHTable));
+	addErr(JSON.stringify(dataStream.Body.toString('utf-8')));
 	if (err) {
 		addErr(err);
 	}; // end if err
