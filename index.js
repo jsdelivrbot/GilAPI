bfb860a23e4ba3d99e5cdf8607ed24259ee8e637
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-02-19 13:01:07 -0800

    Readd quad-parse.

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ var $urlPWHParams = {
 	Key: 'userPWHTable.json'
 };
 $s3.getObject($urlPWHParams, function(err, dataStream){
+	$userPWHTable = JSON.parse(JSON.parse(JSON.parse(JSON.parse(dataStream.Body.toString('utf-8')))));
+	addErr(JSON.stringify($userPWHTable));
 	if (err) {
 		addErr(err);
 	}; // end if err
