12ad633655af0e1052888f0c32f776ff22b54ba1
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-02-19 12:17:23 -0800

    Partial reversion of recent reversions.

--- index.js
+++ index.js
@@ -29,11 +29,13 @@ $s3.createBucket($privateParams);
 
 var $userPWHTable;
 var $urlPWHParams = {
-	Bucket: $privateBucket, 
-	Key: 'userPWHTable.json'
+    Bucket: $privateBucket, 
+    Key: 'userPWHTable.json',
+    ResponseContentType: "application/json"
 };
 $s3.getObject($urlPWHParams, function(err, dataStream){
-	$userPWHTable = dataStream.Body.toString('utf-8');
+	//$userPWHTable = dataStream.Body.toString('utf-8');
+	$userPWHTable = dataStream.Body;
 	addErr(JSON.stringify(dataStream));
 	if (err) {
 		addErr(err);
