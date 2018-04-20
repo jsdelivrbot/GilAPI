84a5993dacf1721aaed7d7dfa8441e5b52da1731
GILNOVO\Gilgamech <StephenGillie@Gilgamech.com>
2018-04-19 17:46:46 -0700

    Revert to last working version.

--- index.js
+++ index.js
@@ -142,6 +142,7 @@ app.post(/\S+/, function(request, response) {
     var $enteredPassword = request.query.password;
 	addErr(("Login for user: " + $userName));
 	
+		addErr(("Searching for user: " + $userName));
 	if ($userPWHTable[$userName]) {
 		$pwhash = $userPWHTable[$userName];
 		addErr(("User found: " + $userName));
@@ -151,11 +152,8 @@ app.post(/\S+/, function(request, response) {
 					addErr($err);
 			}; //end if err
 			if ($userFound) {
-				addErr(("User password matches: " + $userName));
-				request.session.user = $userName;
 				$settingsVar.clientIP = request.ip;
-				$settingsVar.requestInfo = JSON.stringify(request);
-				$settingsVar.session = request.session;
+				addErr(("User password matches: " + $userName));
 				if ($aclTable[$userName]) {
 					var $urlParams = {
 						ContentType: "text/plain;charset=UTF-8",
@@ -232,7 +230,7 @@ app.post(/\S+/, function(request, response) {
 			$settingsVar.awsS3Key = "";
 
 			}; // end if aclTable
-	  }); // end bcrypt.hash
+		}); // end bcrypt.hash
 	}; // end if userPWHTable
 }); // end app post login 
 
